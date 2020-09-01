bda9f60265bb0d6e69df1400490ccf14d6a84681
Jamin Thornsberry <jamin.thorns@gmail.com>
2020-08-31 22:27:43 -0500

    Initial commit

---
+++ lib/ex_html.ex
@@ -0,0 +1,18 @@
+defmodule ExHtml do
+  @moduledoc """
+  Documentation for `ExHtml`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExHtml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_html_test.exs
@@ -0,0 +1,8 @@
+defmodule ExHtmlTest do
+  use ExUnit.Case
+  doctest ExHtml
+
+  test "greets the world" do
+    assert ExHtml.hello() == :world
+  end
+end
